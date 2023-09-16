fd74d728087e5eff3ff4f344c25e43ac78dd52cc
jun <912833721@qq.com>
2023-09-17 04:28:09 +0800

    edit

--- predictive-back-gesture/app/src/main/java/com/hanmajid/android/predictivebackgesture/MainActivity.kt
+++ predictive-back-gesture/app/src/main/java/com/hanmajid/android/predictivebackgesture/MainActivity.kt
@@ -2,11 +2,17 @@ package com.hanmajid.android.predictivebackgesture
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.window.OnBackInvokedCallback
 import android.window.OnBackInvokedDispatcher
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * 主要看AndroidX的情况,注意是[onBackPressedDispatcher]
+ *
+ * 记得xml添加[android:enableOnBackInvokedCallback="true"]属性
+ */
 class MainActivity : AppCompatActivity() {
 
     private lateinit var onBackInvokedCallback: OnBackInvokedCallback
@@ -19,22 +25,24 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             onBackInvokedCallback = OnBackInvokedCallback {
                 // Your app's onBackPressed logic goes here...
+                Log.i("TAG", "Non-AndroidX: onBackInvoked")
             }
-            onBackInvokedDispatcher.registerOnBackInvokedCallback(
-                OnBackInvokedDispatcher.PRIORITY_DEFAULT,
-                onBackInvokedCallback,
-            )
+            // onBackInvokedDispatcher.registerOnBackInvokedCallback(
+            //     OnBackInvokedDispatcher.PRIORITY_DEFAULT,
+            //     onBackInvokedCallback,
+            // )
         }
 
         // For AndroidX:
         val onBackPressedCallback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 // Your app's onBackPressed logic goes here...
+                Log.i("TAG", "AndroidX: handleOnBackPressed")
             }
         }
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
         // You can enable/disable onBackPressedCallback by using `isEnabled` method:
-        // onBackPressedCallback.isEnabled = false
+        onBackPressedCallback.isEnabled = false
     }
 
     override fun onDestroy() {
